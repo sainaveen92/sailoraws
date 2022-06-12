72e74e4d8dc4d352883a88dbd9279d1ccbfb8642
techsriman <tech.sriman.com>
2022-06-12 11:08:12 -0400

    fix

--- main.tf
+++ main.tf
@@ -20,13 +20,13 @@ resource "aws_key_pair" "sailorkeypair" {
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDdndkQHnwjpix3M0H/j6K8GXXFVvKyQU8mI47bALm08vC+g5thV6NgqezBoWTTLtRZePrYlS1aQYRNBxj57SKXLtFrxlLGIBL9es9xf/hYGC4Gy/cWlfstK/RFarKvs4iM6SKTqdnqHSL8geBz8Z2ByReFwKl+Auc64a9IgRHfnJUv5KaF1sOwtJ9x135mCo9I8uyG+Opv37OP6vjEuGt6jTYTPxmHsDsX+sC4mINQJtbajifwQkb9zojO7mRYO1trddm/D71MdGMHaGuZtjlaABQvGBBmF3Z13pojLlIjS6ewS2Cj3ri6FNbOsTW6k0mTr2ngwv0q6j0K0463Ti8sv5S36SqFN9fAT31hZZYpHH6lR0DOGNAcdN5Jwrk7sKxpfM1teknCe4kXr1+GfHhBwgAHAjP38TjpvSikFoSUD6lzC61wHgzsDU2En3tVCx1pdjOwB6NJ4yPcNoIPa1YtjijsHsqn7bp3a2UxhL1lOxsppKppBsp8n3MQlCRHtfM= Sriman@DESKTOP-1QI4S5N"
 }
 resource "aws_security_group" "sailorsg" {
-    ingress = {
+    ingress  {
         from_port  = "8080"  
         to_port = "8080"
         cidr_blocks = ["0.0.0.0/0"]
         protocol = "TCP"
     }
-    egress = {
+    egress  {
       cidr_blocks = [ "0.0.0.0/0" ]
       from_port = 0
       protocol = -1
